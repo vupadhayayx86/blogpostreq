347d29e1228a5104f11045a85c1ff1adf9f4bcd2
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-06-15 13:18:34 +0530

    Initial commit Package JSON 3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('node:http');
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'application/json' });
   console.log(req.method)
   res.end('Hello Server');
   if(req.method=="POST"){
@@ -9,22 +9,24 @@ const server = http.createServer((req, res) => {
     req.on("data",(chunk)=>{
         data+=chunk.toString()
     })
+    console.log(data)
+        req.on("end",()=>{
+        try{
+            const blog=JSON.parse(data)
+            console.log(blog)
+            res.statusCode=200;
+            res.end("Data Received Successfully")
+        }catch(err){
+            res.end("Something went wrong")
+        }
+    })
+  } else {
+    res.statusCode=404
+    res.end()
+
   }
 
-//     req.on("end",()=>{
-//         try{
-//             const blog=JSON.parse(data)
-//             console.log(blog)
-//             res.statusCode=200;
-//             res.end("Data Received Successfully")
-//         }catch(err){
-//             res.end("Something went wrong")
-//         }
-//     })
-//   } else {
-//     res.statusCode=404
-//     res.end()
-//   }
+
 });
 
 server.listen(3001, () => console.log('Server started on port 3000'));
