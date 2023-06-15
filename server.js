2876548063ec0b93834bd916e76095e3a3d6c7d8
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-06-15 12:26:21 +0530

    Initial commit Post request

--- server.js
+++ server.js
@@ -2,6 +2,26 @@ const http = require('node:http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Server');
+  if(req.method=="POST"){
+    let data=""
+    req.on("data",(chunk)=>{
+        data+=chunk.toString()
+    })
+
+    req.on("end",()=>{
+        try{
+            const blog=JSON.parse(data)
+            console.log(blog)
+            res.statusCode=200;
+            res.end("Data Received Successfully")
+        }catch(err){
+            res.end("Something went wrong")
+        }
+    })
+  } else {
+    res.statusCode=404
+    res.end()
+  }
 });
 
 server.listen(3001, () => console.log('Server started on port 3000'));
